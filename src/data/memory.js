a0616012bae562972d6f182ef313e1398b455974
Eduarda <eduarda_geisel@estudante.sesisenai.org.br>
2026-06-18 22:11:52 -0300

    todos

---
+++ src/data/memory.js
@@ -0,0 +1,9 @@
+ const todos = [
+    { id: 1, titulo: "Estudar Node.js", descricao: "tarefa 1", feito: false},
+    { id: 2, titulo: "Fazer exercícios", descricao: "tarefa 2", feito: true},
+    { id: 3, titulo: "Revisar código", descricao: "tarefa 3", feito: false}
+];
+
+module.exports = {
+    todos
+}
---
+++ src/routes.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const { todos } = require("./data/memory")
+
+const router = express.Router()
+
+router.get('/todos', (req,res) => {
+    return res.status(200).json ({
+        tarefas: todos
+    })
+})
+
+router.put('/todos', (req, res) => {
+    r
+})
+
+router.post('/todos', (req, res) => {
+    const { titulo, descricao } = req.body;
+
+    if (!titulo) {
+        return res.status(400).json({ mensagem: "Título é obrigatório"})
+    }
+
+    const novaTarefa = {
+        id: new Date().toString(),
+        titulo,
+        descricao,
+        feito: false
+    }
+
+    todos.push(novaTarefa)
+
+    res.status(201).json({
+        mensagem: "Tarefa criada com sucesso",
+        tarefaCriada: novaTarefa
+    })
+})
+
+module.exports = router;
